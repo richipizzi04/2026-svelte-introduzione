1180dd111ab387d5def259582fbd24c568d7cd80
richipizzi04 <riccardo.pizzigoni@mail.polimi.it>
2026-03-25 17:29:27 +0100

    add bottone con la variabile count e raddoppiata con variabile doubled

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,17 @@
 <script> //scrivo il javascript qui dentro
     console.log("Hello world");
+    let count = $state(0); //è lo stato del comonente, se cambia in qualche modo, il componente si aggiorna
+    let doubled = $derived(count * 2); 
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<button onclick={() => count++}>
+    clicks: {count} <!--stampo il valore di count, ogni volta che clicco sul bottone, count aumenta di 1 e quindi il componente si aggiorna e mostra il nuovo valore di count-->
+</button>
+
+<span>doubled is {doubled}</span>
 
 <style>
     h1 {
