785880a54b19faf5d3560fdd27b5d083d374e9b6
richipizzi04 <riccardo.pizzigoni@mail.polimi.it>
2026-03-25 17:53:30 +0100

    ho creato un componente clickcounter, l'ho collegato in un alltro file e ora posso importanrlo in qualsiasi pagina come se fosse un componente su figma

---
+++ src/components/ClicksCounter.svelte
@@ -0,0 +1,13 @@
+<script> //scrivo il javascript qui dentro
+    let props = $props(); //props è un oggetto che contiene tutte le proprietà che vengono passate al componente, se ne passo una nuova, il componente si aggiorna
+    let count = $state(props.initialCount); //è lo stato del comonente, se cambia in qualche modo, il componente si aggiorna, in questo caso inizia da initialCount
+    let doubled = $derived(count * 2); 
+</script>
+
+<h1>Clicks Counter</h1>
+
+<button onclick={() => count++}>
+    clicks: {count} <!--stampo il valore di count, ogni volta che clicco sul bottone, count aumenta di 1 e quindi il componente si aggiorna e mostra il nuovo valore di count-->
+</button>
+
+<span>doubled is {doubled}</span>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,13 @@
 <script> //scrivo il javascript qui dentro
-    console.log("Hello world");
-    let count = $state(0); //è lo stato del comonente, se cambia in qualche modo, il componente si aggiorna
-    let doubled = $derived(count * 2); 
+  
+    import ClicksCounter from "../components/ClicksCounter.svelte"; //sono le due componenti che ho creato e le importo nella
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-<button onclick={() => count++}>
-    clicks: {count} <!--stampo il valore di count, ogni volta che clicco sul bottone, count aumenta di 1 e quindi il componente si aggiorna e mostra il nuovo valore di count-->
-</button>
-
-<span>doubled is {doubled}</span>
+<ClicksCounter initialCount={5}/> 
+<ClicksCounter initialCount={3}/> 
 
 <style>
     h1 {
