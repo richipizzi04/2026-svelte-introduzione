6f9394b5bb808867c77919c7aad95a61e80a21c8
richipizzi04 <riccardo.pizzigoni@mail.polimi.it>
2026-03-25 17:12:37 +0100

    add style local and global

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,4 +8,14 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children()}
+{@render children()} 
+
+<footer>
+	<p>polimi 2026</p>
+</footer>
+
+<style> /*dice di trattare questo style css globalmente quindi applicato a tutti i p paragrafi*/
+	:global(p){
+        color: rgb(255, 164, 104);
+    }
+</style>
