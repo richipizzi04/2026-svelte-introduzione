0683e44380af4e7c59d7dcec72504e72d6a19164
richipizzi04 <riccardo.pizzigoni@mail.polimi.it>
2026-04-08 18:10:53 +0200

    ho creato lo state.svelte e lìho messo tutto nella page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,19 @@
 <script> //scrivo il javascript qui dentro
+    import {website} from "../components/state.svelte.js"; //importo lo stato globale del sito, in questo modo posso usarlo in tutti i componenti che lo importano, se cambio il titolo in uno dei componenti, cambia anche negli altri componenti che lo usano
     import ClicksCounter from "../components/ClicksCounter.svelte"; //sono le due componenti che ho creato e le importo nella pagina principale, in questo modo posso usarle come se fossero dei tag html
     import CanvasSquare from "../components/CanvasSquare.svelte"; //per fare comparire la linea sella size e il colore
     import ToDoList from "../components/ToDoList.svelte";
+    import TitleEditor from "../components/TitleEditor.svelte";
 
     let count = $state(5)
     let doubled = count * 2;
 
 </script>
 
-<h1>Welcome to SvelteKit</h1>
+<h1> {website.title} </h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<TitleEditor/>
 
 <ClicksCounter bind:initialCount={count}/> 
 {#if count > 10 && count < 20}
