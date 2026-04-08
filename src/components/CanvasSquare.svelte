ff069a8a602ded6d25d2bac5f33e24c0f901e183
richipizzi04 <riccardo.pizzigoni@mail.polimi.it>
2026-04-08 17:20:01 +0200

    ho messo il bind ai label

--- src/components/CanvasSquare.svelte
+++ src/components/CanvasSquare.svelte
@@ -18,14 +18,15 @@
 
 <h3>Canvas Square</h3>
 
+<!--il bind serve per collegare gli elementi, lo collego a ciò che sta dentro le graffe {}-->
 <article>
     <canvas bind:this={canvas} width="100" height="100"></canvas> <!--è uno spazio vuoto che ci permete di disegnare ogni singolo pixel, usato per fare disegni 2d, mettere disegni 3d, fare grafici, ...-->
     <nav> <!--ci metto dei controlli per gestire il disegno ()il quadrato creato sopra-->
         <label> <!--serve per poter selezionare l'input anche senza che io schiaccio sull'input stesso-->
-            Size: <input type="range">
+            Size: <input type="range" bind:value={size}>
         </label>
         <label>
-            Color: <input type="color">
+            Color: <input type="color" bind:value={color}> <!--grazie al bind collego il colore alla variabile color che ho scritto nello script-->
         </label>
     </nav>
 </article>
