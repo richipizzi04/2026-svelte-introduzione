ec6a57202736c6c0183ca3dfe596d3abc16277bb
richipizzi04 <riccardo.pizzigoni@mail.polimi.it>
2026-04-08 17:59:05 +0200

    migliorare todos con l'input chckbox

--- src/components/ToDoList.svelte
+++ src/components/ToDoList.svelte
@@ -1,7 +1,7 @@
 <script>
     let todos = $state([
         { title: "Assignment 1", done: false },
-        { title: "Assignment 2", done: true },
+        { title: "Assignment 2", done: false },
         { title: "Assignment 3", done: false }
     ]); //è un array di oggetti, ogni oggetto rappresenta un to do con un titolo e un booleano che indica se è stato fatto o no 
 </script>
@@ -10,6 +10,8 @@
 
 <ul>
     {#each todos as todo} <!--per ogni elemento dell'array todos, creo un elemento li con il titolo del to do e una checkbox che indica se è stato fatto o no--> 
-        <li>{todo.title}</li>
+        <li>
+            <input type="checkbox" bind:checked={todo.done}> <!--grazie al bind, quando cambio lo stato della checkbox, cambia anche il valore di done del to do corrispondente-->
+            {todo.title}</li>
     {/each}         
 </ul>
